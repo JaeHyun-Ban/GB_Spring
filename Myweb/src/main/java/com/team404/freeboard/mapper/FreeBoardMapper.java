5c409714cfd8ab42213ebb99c85552646ca63434
Ban-dalgom <wogus0008@gmail.com>
2020-12-18 03:29:00 +0900

    페이징처리(어려움)

--- Myweb/src/main/java/com/team404/freeboard/mapper/FreeBoardMapper.java
+++ Myweb/src/main/java/com/team404/freeboard/mapper/FreeBoardMapper.java
@@ -3,17 +3,22 @@ package com.team404.freeboard.mapper;
 import java.util.ArrayList;
 
 import com.team404.command.FreeBoardVO;
+import com.team404.common.util.Criteria;
 
 public interface FreeBoardMapper {
 
 	// 추상메서드 선언(service를 그대로 가져옴)
-	public void regist(FreeBoardVO vo); //게시글 등록
+	public void regist(FreeBoardVO vo); // 게시글 등록
 
-	public ArrayList<FreeBoardVO> getList(); //게시글 가져오기
+//	public ArrayList<FreeBoardVO> getList(); //게시글 가져오기(기존)
+	public ArrayList<FreeBoardVO> getList(Criteria cri); // 게시글 가져오기(페이징)
+	
+	public int getTotal(Criteria cri);//전체 게시글 수 가져오기
 
-	public FreeBoardVO getContent(int bno); //상세 게시글 보기
+	public FreeBoardVO getContent(int bno); // 상세 게시글 보기
 
 	public int update(FreeBoardVO vo);
 
 	public int delete(int bno);
+
 }
--- Myweb/src/main/java/com/team404/freeboard/service/FreeBoardService.java
+++ Myweb/src/main/java/com/team404/freeboard/service/FreeBoardService.java
@@ -3,16 +3,22 @@ package com.team404.freeboard.service;
 import java.util.ArrayList;
 
 import com.team404.command.FreeBoardVO;
+import com.team404.common.util.Criteria;
 
 public interface FreeBoardService {
+
+	// 추상메서드 선언
+	public void regist(FreeBoardVO vo);// 게시글 등록
+
+//	public ArrayList<FreeBoardVO> getList();// 게시글 가져오기
+	public ArrayList<FreeBoardVO> getList(Criteria cri);// 게시글 가져오기
 	
-	//추상메서드 선언
-	
-	public void regist(FreeBoardVO vo);//게시글 등록
-	public ArrayList<FreeBoardVO>getList();//게시글 가져오기
-	public FreeBoardVO getContent(int bno);//상세게시글보기
+	public int getTotal(Criteria cri);//전체 게시글 수 가져오기
+
+	public FreeBoardVO getContent(int bno);// 상세게시글보기
+
 	public int update(FreeBoardVO vo);
+
 	public int delete(int bno);
-	
-	
+
 }
